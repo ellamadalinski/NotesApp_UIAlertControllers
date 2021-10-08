67115e707c07a226f077235ffb2bf2367d57beb4
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-07 22:24:32 -0500

    initial and final commit

--- NotesApp_UIAlertControllers/ViewController.swift
+++ NotesApp_UIAlertControllers/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextFieldOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func submitAction(_ sender: UIButton) {
+        //1 Create Alert Controller
+        let alert = UIAlertController(title: "Error!", message: "Not a valid name", preferredStyle: .alert)
+        
+        //2 Create Actions
+        let okAction = UIAlertAction(title: "Ok", style: .destructive, handler: nil)
+        let noAction = UIAlertAction(title: "No", style: .default) { action in
+            self.nameTextFieldOutlet.text = ""
+        }
+        
+        //3 Add Actions
+        alert.addAction(okAction)
+        alert.addAction(noAction)
+        
+        //4 Present Alert
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
 
 }
 
